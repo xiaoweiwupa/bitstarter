dab12e247ae414c869edeec461213710f350ce79
Xiaowei Wu <xiaoweiwupa@yahoo.com>
2014-01-13 07:10:40 +0000

    one more

--- web.js
+++ web.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var out = Buffer.toString (fs.readFileSync('index.html'));
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
